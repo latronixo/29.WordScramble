419f89c1690aeacbe108ce32ce07d8e37c33cca6
Valentin <latronixo@gmail.com>
2025-05-27 14:18:11 +0300

    29. 3/6 Демонстрация проверки строки на наличие несуществующих слов (в английском языке)

--- 29.WordScramble/ContentView.swift
+++ 29.WordScramble/ContentView.swift
@@ -10,26 +10,20 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        List {
-            Text("Статическая строка")
-            ForEach(testStrings(), id: \.self){
-                Text($0)
-            }
-            Text("Статическая строка")
-        }
+        Text(String(testStrings()))
     }
-    func testStrings() -> [String] {
-        let input = """
-               a       d    
-              b          d   
-               c         d    
-            """
-        let letters = input.components(separatedBy: "\n")
-        let letter = letters.randomElement()
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-        return [trimmed ?? ""]
+    func testStrings() -> Bool {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        return allGood
     }
 }
+
+
 
 #Preview {
     ContentView()
