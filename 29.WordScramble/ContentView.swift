a164a163d46fa53e102cd18c1d819a1202ff399c
Valentin <latronixo@gmail.com>
2025-05-27 12:00:17 +0300

    Пример динамического List без ForEach

--- 29.WordScramble/ContentView.swift
+++ 29.WordScramble/ContentView.swift
@@ -9,22 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Static Row 1")
-                Text("Static Row 2")
-            }
-
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic Row \($0)")
-                }
-            }
-
-            Section("Section 3") {
-                Text("Static Row 3")
-                Text("Static Row 4")
-            }
+        List (0..<5) {
+            Text("Dynamic Row \($0)")
         }
     }
 }
