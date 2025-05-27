f0087ccd7140e20ce40da21bdb9dc7108af4e6f2
Valentin <latronixo@gmail.com>
2025-05-27 11:42:31 +0300

    Initial Commit

---
+++ 29.WordScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  29.WordScramble
+//
+//  Created by Валентин on 27.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 29.WordScramble/_9_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_WordScrambleApp.swift
+//  29.WordScramble
+//
+//  Created by Валентин on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct _9_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
